b19b0da4ca04d160e1f980da8c001293eaeac519
Karma Jigme Wangchuk <karmajigme.wangchuk@my.jcu.edu.au>
2024-02-12 20:09:13 +1000

    Add score status program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,4 +1,26 @@
 """
-CP1404 - Practical 1
+CP1404 - Practical 2
 Student name: Karma Jigme Wangchuk
 """
+
+SECRET = 5
+def main():
+    password = get_password(SECRET)
+    print_asterisks(password)
+
+
+def get_password(password_length):
+    """Get password , ensuring it meets the least number of characters."""
+    password = input(f"Enter password of at least {password_length} characters: ")
+    while len(password) < password_length:
+        print("Password too short")
+        password = input(f"Enter password of at least {password_length} characters: ")
+    return password
+
+
+def print_asterisks(num):
+    """Print asterisks corresponding to the password's length"""
+    print('*' * len(num))
+
+
+main()
---
+++ prac_02/score.py
@@ -0,0 +1,25 @@
+"""
+CP1404 - Practical 2
+Student name: Karma Jigme Wangchuk
+"""
+
+
+def main():
+    """Get a score and display its category."""
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    """Determine the status of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
